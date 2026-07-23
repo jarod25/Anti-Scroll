87f0bddb39e7b0d7efacda4cf2b9a6755ac90316
Jarod KOHLER <kohler.jarod.2004@gmail.com>
2026-07-24 00:44:06 +0200

    test: verify Hilt application registration

--- app/src/androidTest/java/fr/jarodkohler/antiscroll/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/fr/jarodkohler/antiscroll/ExampleInstrumentedTest.kt
@@ -1,24 +1,20 @@
 package fr.jarodkohler.antiscroll
 
-import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Assert.assertTrue
 import org.junit.Test
 import org.junit.runner.RunWith
 
-import org.junit.Assert.*
-
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+class ApplicationBootstrapInstrumentedTest {
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("fr.jarodkohler.antiscroll", appContext.packageName)
+    fun registeredApplicationUsesHiltBootstrap() {
+        val application = InstrumentationRegistry
+            .getInstrumentation()
+            .targetContext
+            .applicationContext
+
+        assertTrue(application is AntiScrollApplication)
     }
 }
