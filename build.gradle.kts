3297ae6f9dabf1f73a9313f48011688a42537c5a
Jarod KOHLER <kohler.jarod.2004@gmail.com>
2026-07-25 18:41:16 +0200

    fix: stabilize Spotless line endings on Windows

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import com.diffplug.spotless.LineEnding
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.spotless)
@@ -11,6 +13,8 @@ plugins {
 }
 
 spotless {
+    lineEndings = LineEnding.UNIX
+
     kotlin {
         target("**/*.kt")
         targetExclude("**/build/**", "**/.gradle/**")
