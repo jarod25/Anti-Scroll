4850d7428fc2e587db9402fdd711c3ba1a68f54b
Jarod KOHLER <kohler.jarod.2004@gmail.com>
2026-07-27 22:31:17 +0200

    Apply Kotlin formatting

--- domain/src/main/kotlin/fr/jarodkohler/antiscroll/domain/application/MonitoredApplication.kt
+++ domain/src/main/kotlin/fr/jarodkohler/antiscroll/domain/application/MonitoredApplication.kt
@@ -3,8 +3,4 @@ package fr.jarodkohler.antiscroll.domain.application
 import java.time.Instant
 
 /** User-selected application configuration without Android presentation metadata. */
-data class MonitoredApplication(
-    val packageName: ApplicationPackageName,
-    val isEnabled: Boolean,
-    val addedAt: Instant
-)
+data class MonitoredApplication(val packageName: ApplicationPackageName, val isEnabled: Boolean, val addedAt: Instant)
