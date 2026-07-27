408833161489942cbdb4b983776d43d92829ed99
Jarod KOHLER <kohler.jarod.2004@gmail.com>
2026-07-27 22:22:36 +0200

    Add observation domain contracts

---
+++ domain/src/main/kotlin/fr/jarodkohler/antiscroll/domain/application/MonitoredApplication.kt
@@ -0,0 +1,10 @@
+package fr.jarodkohler.antiscroll.domain.application
+
+import java.time.Instant
+
+/** User-selected application configuration without Android presentation metadata. */
+data class MonitoredApplication(
+    val packageName: ApplicationPackageName,
+    val isEnabled: Boolean,
+    val addedAt: Instant
+)
