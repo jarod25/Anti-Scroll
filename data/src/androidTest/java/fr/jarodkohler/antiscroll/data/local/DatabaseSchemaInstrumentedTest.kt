05aad557795e0d2d486df74805bdf6fb535703b6
Jarod Kohler <kohler.jarod.2004@gmail.com>
2026-07-28 18:09:57 +0200

    Test Room migration from version one

--- data/src/androidTest/java/fr/jarodkohler/antiscroll/data/local/DatabaseSchemaInstrumentedTest.kt
+++ data/src/androidTest/java/fr/jarodkohler/antiscroll/data/local/DatabaseSchemaInstrumentedTest.kt
@@ -3,6 +3,7 @@ package fr.jarodkohler.antiscroll.data.local
 import androidx.room.testing.MigrationTestHelper
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
 import org.junit.Rule
 import org.junit.Test
@@ -17,15 +18,49 @@ class DatabaseSchemaInstrumentedTest {
     )
 
     @Test
-    fun exportedVersionOneSchemaCanBeCreated() {
-        val database = migrationHelper.createDatabase(TEST_DATABASE, AntiScrollDatabaseSchema.VERSION)
+    fun migrationOneToTwoPreservesConfigurationAndCreatesObservationTables() {
+        migrationHelper.createDatabase(TEST_DATABASE, 1).apply {
+            execSQL(
+                "INSERT INTO monitored_applications " +
+                    "(package_name, is_enabled, added_at_epoch_millis) " +
+                    "VALUES ('com.instagram.android', 1, 1000)"
+            )
+            close()
+        }
+
+        val database = migrationHelper.runMigrationsAndValidate(
+            TEST_DATABASE,
+            2,
+            true,
+            DatabaseMigrations.MIGRATION_1_2
+        )
 
         try {
             database.query(
-                "SELECT name FROM sqlite_master " +
-                    "WHERE type = 'table' AND name = 'monitored_applications'"
+                "SELECT is_enabled, added_at_epoch_millis FROM monitored_applications " +
+                    "WHERE package_name = 'com.instagram.android'"
             ).use { cursor ->
                 assertTrue(cursor.moveToFirst())
+                assertEquals(1, cursor.getInt(0))
+                assertEquals(1_000L, cursor.getLong(1))
+            }
+
+            val expectedTables = setOf(
+                "usage_events",
+                "daily_application_usage",
+                "collection_checkpoints",
+                "collection_gaps",
+                "monitoring_health"
+            )
+            database.query(
+                "SELECT name FROM sqlite_master WHERE type = 'table'"
+            ).use { cursor ->
+                val actualTables = buildSet {
+                    while (cursor.moveToNext()) {
+                        add(cursor.getString(0))
+                    }
+                }
+                assertTrue(actualTables.containsAll(expectedTables))
             }
         } finally {
             database.close()
