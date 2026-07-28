9e111359929e5f383a82efdb4a5fedcdcf99f2be
Jarod KOHLER <kohler.jarod.2004@gmail.com>
2026-07-28 15:15:07 +0200

    Expose enabled monitored applications

--- data/src/main/java/fr/jarodkohler/antiscroll/data/local/MonitoredApplicationDao.kt
+++ data/src/main/java/fr/jarodkohler/antiscroll/data/local/MonitoredApplicationDao.kt
@@ -10,6 +10,9 @@ interface MonitoredApplicationDao {
     @Query("SELECT * FROM monitored_applications ORDER BY package_name")
     fun observeAll(): Flow<List<MonitoredApplicationEntity>>
 
+    @Query("SELECT * FROM monitored_applications WHERE is_enabled = 1 ORDER BY package_name")
+    suspend fun findEnabled(): List<MonitoredApplicationEntity>
+
     @Query("SELECT * FROM monitored_applications WHERE package_name = :packageName LIMIT 1")
     suspend fun findByPackageName(packageName: String): MonitoredApplicationEntity?
 
