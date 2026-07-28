671348b1bf5e254e060e17cd72582051fbaf5ba9
Jarod KOHLER <kohler.jarod.2004@gmail.com>
2026-07-28 16:57:07 +0200

    Bind usage stats monitoring source

---
+++ monitoring/src/main/java/fr/jarodkohler/antiscroll/monitoring/usagestats/UsageStatsMonitoringModule.kt
@@ -0,0 +1,25 @@
+package fr.jarodkohler.antiscroll.monitoring.usagestats
+
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import dagger.multibindings.IntoSet
+import fr.jarodkohler.antiscroll.domain.observation.UsageObservationSource
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class UsageStatsMonitoringModule {
+    @Binds
+    @Singleton
+    abstract fun bindUsageStatsEventGateway(
+        gateway: AndroidUsageStatsEventGateway
+    ): UsageStatsEventGateway
+
+    @Binds
+    @IntoSet
+    abstract fun bindUsageStatsObservationSource(
+        source: UsageStatsObservationSource
+    ): UsageObservationSource
+}
