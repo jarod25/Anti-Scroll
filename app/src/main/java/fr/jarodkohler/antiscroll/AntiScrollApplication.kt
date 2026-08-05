f4f78e6ff61e27472bcb3481aeb4a600f577344d
Jarod Kohler <kohler.jarod.2004@gmail.com>
2026-08-05 18:13:05 +0200

    Start shared session runtime with application

--- app/src/main/java/fr/jarodkohler/antiscroll/AntiScrollApplication.kt
+++ app/src/main/java/fr/jarodkohler/antiscroll/AntiScrollApplication.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import androidx.hilt.work.HiltWorkerFactory
 import androidx.work.Configuration
 import dagger.hilt.android.HiltAndroidApp
+import fr.jarodkohler.antiscroll.restriction.SharedSessionRuntime
 import javax.inject.Inject
 
 @HiltAndroidApp
@@ -12,6 +13,14 @@ class AntiScrollApplication :
     Configuration.Provider {
     @Inject
     lateinit var workerFactory: HiltWorkerFactory
+
+    @Inject
+    lateinit var sharedSessionRuntime: SharedSessionRuntime
+
+    override fun onCreate() {
+        super.onCreate()
+        sharedSessionRuntime.start()
+    }
 
     override val workManagerConfiguration: Configuration
         get() = Configuration.Builder()
