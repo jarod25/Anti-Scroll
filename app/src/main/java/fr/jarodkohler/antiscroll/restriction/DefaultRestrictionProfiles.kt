fe13a7d273096b9a2fe04297c26995a049d3ba72
Jarod Kohler <kohler.jarod.2004@gmail.com>
2026-08-05 18:23:28 +0200

    Rename default restriction profiles file

---
+++ app/src/main/java/fr/jarodkohler/antiscroll/restriction/DefaultRestrictionProfiles.kt
@@ -0,0 +1,12 @@
+package fr.jarodkohler.antiscroll.restriction
+
+import fr.jarodkohler.antiscroll.domain.restriction.RestrictionProfile
+import fr.jarodkohler.antiscroll.domain.restriction.RestrictionProfileIdentifier
+
+object DefaultRestrictionProfiles {
+    val observation = RestrictionProfile(
+        identifier = RestrictionProfileIdentifier("observation"),
+        version = 1,
+        sharedSessionPolicy = null
+    )
+}
